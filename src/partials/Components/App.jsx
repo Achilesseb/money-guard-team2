df95d2ca9af459806f6d1ef2cc2be0f99ef68757
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-06-23 20:11:21 +0300

    Trigger build

--- src/partials/Components/App.jsx
+++ src/partials/Components/App.jsx
@@ -7,16 +7,17 @@ import Register from '../Pages/RegisterPage';
 
 function App() {
   return (
-    <HashRouter basename="/money-guard-team2">
-      <Routes>
-        <Route path="/" element={<BackupHome />} />
-        <Route path="/home" element={<BackupHome />} />
-        {/* <Route
-          path="/register"
-          element={<RestrictedRoute component={Register} redirectTo="/home" />}
-        /> */}
-      </Routes>
-    </HashRouter>
+    <Register />
+    // <HashRouter basename="/money-guard-team2">
+    //   <Routes>
+    //     <Route path="/" element={<BackupHome />} />
+    //     <Route path="/home" element={<BackupHome />} />
+    //     {/* <Route
+    //       path="/register"
+    //       element={<RestrictedRoute component={Register} redirectTo="/home" />}
+    //     /> */}
+    //   </Routes>
+    // </HashRouter>
   );
 }
 
