8458b790c026b032bc1be23d93dacb551e43a25b
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-06-23 19:57:41 +0300

    Trigger build

--- src/partials/Components/App.jsx
+++ src/partials/Components/App.jsx
@@ -7,7 +7,7 @@ import Register from '../Pages/RegisterPage';
 
 function App() {
   return (
-    <HashRouter basename="/money-guard-team-2">
+    <HashRouter basename="/money-guard-team2">
       <Routes>
         <Route path="/" element={<BackupHome />} />
         <Route path="/home" element={<BackupHome />} />
